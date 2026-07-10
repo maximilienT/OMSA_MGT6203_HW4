0822d814c9a9b2613eee9d9a9ff181864824efd1
Max <maxthacker1@gmail.com>
2026-07-09 21:40:50 -0700

    Initial commit

---
+++ OMSA_MGT6203_HW4.2.R
@@ -0,0 +1,14 @@
+mydata <- read.csv("ShoppingVisits.csv")
+# Task 1: K-means and scaling
+
+# Task 2: K-means with varied number of clusters
+
+# Task 3: Choosing K
+
+# Task 4: Hierarchical Clustering
+
+# Task 5: K-means Vs heirarchical
+
+# Task 6: Heirarchical with different linkages
+
+# Task 7: Interpret clusers
