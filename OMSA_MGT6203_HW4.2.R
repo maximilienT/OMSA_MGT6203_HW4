3edee6bd7c241005732e4624438342b85ff804eb
Max <maxthacker1@gmail.com>
2026-07-09 22:46:33 -0700

    Finished

--- OMSA_MGT6203_HW4.2.R
+++ OMSA_MGT6203_HW4.2.R
@@ -54,10 +54,38 @@ plot(ss.vec, type='b', xlab="Number of Clusters", ylab = "Total Within-Cluster S
 ### Task 4: Hierarchical Clustering ###
 
 # 10. Perform hierarchical clustering, average linkage
+hc.res.average <- hclust(dist(mydata.sc), method = "average")
+plot(hc.res.average, cex=0.5, main="Dendrogram (Average Linkage)",xlab="",ylab="",sub="")
 
+# 11. Cut into 3 clusters
+abline(h=1.75, col='red', lty=2)
 
 ### Task 5: K-means Vs heirarchical ###
 
+# 12. compare 3 clusters of both meathods
+par(mfrow=c(1,2))
+plot(mydata, col=km.res.ns.2$cluster, pch=20, main=paste("K-means with 3 Clusters"))
+plot(mydata, col=cutree(hc.res.average, k=3), pch=20, main="Heirarchical Clustering (Average Linkage)")
+
+# 13. it looks like the two outcomes are slightly different.
+
 ### Task 6: Heirarchical with different linkages ###
 
+# 14. perform the remaing 3 heirarchical clustering and compare.
+hc.res.centroid <- hclust(dist(mydata.sc), method = "centroid")
+hc.res.complete <- hclust(dist(mydata.sc), method = "complete")
+hc.res.single <- hclust(dist(mydata.sc), method = "single")
+
+par(mfrow=c(2,2))
+plot(mydata, col=cutree(hc.res.average, k=3), pch=20, main="Heirarchical Clustering (Average Linkage)")
+plot(mydata, col=cutree(hc.res.complete, k=3), pch=20, main="Heirarchical Clustering (Complete Linkage)")
+plot(mydata, col=cutree(hc.res.single, k=3), pch=20, main="Heirarchical Clustering (Single Linkage)")
+plot(mydata, col=cutree(hc.res.centroid, k=3), pch=20, main="Heirarchical Clustering (Centroid Linkage)")
+
+# 15. Single looks to be the most unreasonable, it has one very large cluster with another only having 2 points
+
 ### Task 7: Interpret clusers ###
+
+# 16. The three clusters seem to be low visits with low spending, low visits with high spending, and high visits with high spending
+
+# 17. I think marketing resources should be directed to the low visits high spending group. If you could get them to visit more often they might spend even more.
